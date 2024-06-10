0f6be6a53fb8d809f0e36025daeeecf91bf51348
j b <j.is.brown@gmail.com>
2024-06-10 12:20:16 -0500

    added input form to html

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,6 @@
 const APIKey = '5bc593d5b6a903eb1bcff74b89d2892a';
 
-function getLaction(cityName){
+function getLoction(cityName){
     const APIUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${APIKey}'
     fetch(APIUrl).then(function (response){
         return response.json()
@@ -12,11 +12,11 @@ function getLaction(cityName){
 }
 
 function getWeather(lat, lon){
-    const APIUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}'
+    const APIUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${APIkey}'
     fetch(APIUrl).then(function (response){
         return response.json()
     }).then(function(data){
         console.log(data)
     })
 }
-getLaction('lexington, ky');
+getLocation('lexington');
