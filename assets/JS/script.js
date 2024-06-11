d17e3d2afd8478b6fc10429838e470062b60111a
j b <j.is.brown@gmail.com>
2024-06-10 19:20:41 -0500

    pulled data from console and created divs to append to page. this shows the weather for today and future forecast

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,4 @@
-let cities = JSON.parse(localStorage.getItem('cities')) || [];
+let cities = JSON.parse(localStorage.getItem("cities")) || [];
 const cityNameEl = $('#cityName');
 const apiKey = '5bc593d5b6a903eb1bcff74b89d2892a';
 //server side api function for weather by city
@@ -7,9 +7,23 @@ function getWeather(cityName) {
     fetch(apiUrl).then(function (response) {
         return response.json()
     }).then(function (data) {
+        
         console.log(data)
+        const temp = data.main.temp;
+        const wind = data.wind.speed;
+        const humidity = data.main.humidity;
+        const todaysForecast = `
+        <div>
+            <h4>${cityName}</h4>
+            <p>${temp}</p>
+            <p>${wind}</p>
+            <p>${humidity}</p>
+        </div>
+        `
+        $('#weatherDisplay').append(todaysForecast);
+
     })
-}
+} 
 
 function getForecast(cityName) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
@@ -22,21 +36,41 @@ function getForecast(cityName) {
         for (let i = 0; i < forecasts.length; i = i + 8) {
             const forecast = forecasts[i];
             console.log(forecast);
+
+            const temp = forecast.main.temp;
+            const wind = forecast.wind.speed;
+            const humidity = forecast.main.humidity;
+            const futureCast = `
+            <div>
+                <h4>${cityName}
+                <p>${temp}</p>
+                <p>${wind}</p>
+                <p>${humidity}</p>
+            </div>
+            `
+            $('#forecastDisplay').append(futureCast);
         }
     })
 }
 
+//creating a card to append to the search history
 function createCityCard(task) {
     const cityCardEl = $('<div class="task card mb-3 p-3">');
-    const cityNameEl = $('<h3').text(task.cityName);
+    const cityNameEl = $('<div>').text(task.cityName);
 
-    $('#city-history').append(cityCardEl);
+    $('#cityHistory').append(cityCardEl);
     cityCardEl.append(cityNameEl);
 }
 
+function renderCities() {
+    for (let city of cities) {
+        createCityCard(city);
+    }
+}
 //function to add input data and convert it to an array in local storage
-function addCitySearch(event) {
+function addCityList(event) {
     event.preventDefault();
+
     if (!cities) {
         cities = [];
     }
@@ -49,14 +83,14 @@ function addCitySearch(event) {
 
     localStorage.setItem('cities', JSON.stringify(cities));
 
+    const { cityName } = city;
+
+    getWeather(cityName);
+    getForecast(cityName);
 
 }
-$("#add-city").on('submit', addCitySearch);
 
-getWeather('lexington');
-getForecast('lexington');
-searchCity('lexington');
-
-
-
-//event delegation
+$(document).ready(function () {
+    renderCities();
+})
+$("#add-city").on('submit', addCityList);
